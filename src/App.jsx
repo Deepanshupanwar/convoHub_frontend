352d00fbbe71a288cd255a0380f4006036556288
Deepanshupanwar <deepanshup00@gmail.com>
2025-06-13 22:39:54 +0530

    added analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import MainPage from "./Pages/MainPage";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 import OAuthSuccess from "./Pages/OauthSuccess";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App(){
     return (
@@ -15,6 +16,8 @@ export default function App(){
                 <Route path="/mainPage" element= {<MainPage/>}/>
                 <Route path="/oauth-success" element={<OAuthSuccess/>}/>
             </Routes>
+            
+            <Analytics/>
         </BrowserRouter>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import { ChatProvider } from './Context/chatContext.jsx'
 import { UserProvider } from './Context/userContext.jsx'
 import { GroupProvider } from './Context/groupContext.jsx'
 import { UtilityProvider } from './Context/utilityContext.jsx'
-import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -15,7 +14,6 @@ createRoot(document.getElementById('root')).render(
         <GroupProvider>
           <UtilityProvider>
             <App/>
-            <Analytics/>
           </UtilityProvider>
         </GroupProvider>
       </ChatProvider>
